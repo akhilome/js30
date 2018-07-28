44176f8a23680b3c2ac8e865195f9d706dcb0d99
Kizito Akhilome <kizito@akhilo.me>
2018-07-28 18:31:38 +0100

    Implement day 11 fullscreen button challenge

--- 11/assets/scripts.js
+++ 11/assets/scripts.js
@@ -6,6 +6,7 @@ const progressBar = player.querySelector('.progress__filled');
 const toggle = player.querySelector('.toggle');
 const skipButtons = player.querySelectorAll('[data-skip]');
 const ranges = player.querySelectorAll('.player__slider');
+const fullscreen = player.querySelector('.fullscreen');
 
 // Build out functions
 
@@ -20,7 +21,7 @@ function togglePlay() {
 function updateButton() {
   const icon = this.paused ? '►' : '||';
   toggle.textContent = icon;
-  console.log('Update the button');
+  // console.log('Update the button');
 }
 
 function skip() {
@@ -44,6 +45,10 @@ function scrub(e) {
   // console.log(e);
 }
 
+function toggleFullscreen() {
+  video.webkitRequestFullscreen();
+}
+
 // Hook up to event listeners
 video.addEventListener('click', togglePlay);
 video.addEventListener('play', updateButton);
@@ -61,3 +66,5 @@ progress.addEventListener('click', scrub);
 progress.addEventListener('mousemove ', (e) => mouseDown && scrub(e));
 progress.addEventListener('mousedown',  () => mouseDown = true);
 progress.addEventListener('mouseup',  () => mouseDown = true);
+
+fullscreen.addEventListener('click', toggleFullscreen);
